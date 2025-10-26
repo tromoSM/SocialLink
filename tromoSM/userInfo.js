49ab008c7c9053f923ab694b9f119c5df22e041e
tromoSM <tromosm7@gmail.com>
2025-10-26 06:54:28 +0530

    tromoSM/userInfo.js

---
+++ tromoSM/userInfo.js
@@ -0,0 +1,18 @@
+username="tromoSM" //github username
+icotheme="ffffff" //must me hex do not leave whatever u call "#"
+tromoSM_icotheme="white" //leave blank for default | set value to "white" or "light" --case insetive btw
+
+var PROFILE_LINK = "https://github.com/tromoSM" //link when the pfp is clicked
+var PROFILE_LINK_DES="Github profile" //what does the profile link open 
+var description 
+var GITHUB="https://github.com/tromoSM" 
+var LINKTREE="https://linkTr.ee/TromoSM" 
+var INSTAGRAM="https://instagram.com/tromoSM" 
+var X="https://x.com/tromoSM"
+var THREADS="https://www.threads.com/tromoSM"
+var GMAIL="mailto:tromosm.business@gmail.com"
+var LINKEDIN="https://www.linkedin.com/in/tromoSM"
+var BUYMEACOFFEE="https://buymeacoffee.com/tromosm"
+var PONTOON="https://pontoon.mozilla.org/contributors/czwiZRZhsQKfq6_a1Dy2r_PnkUM"
+var SM$PY="https://github.com/tromoSM/SM.S-PY" //personal
+var MOZILLA_SUPPORT="https://support.mozilla.org/en-US/user/tromoSM/"
